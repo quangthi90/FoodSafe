1a2bec4ae0493611666652612c19356268237839
Bommer <quangthi_90@yahoo.com.vn>
2015-07-08 12:45:33 +0700

    - add config file
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Foodsafe/admin/');
+define('HTTP_CATALOG', 'http://localhost/Foodsafe/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Foodsafe/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Foodsafe/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Foodsafe/admin/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Foodsafe/system/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Foodsafe/admin/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Foodsafe/admin/view/template/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Foodsafe/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Foodsafe/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Foodsafe/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Foodsafe/system/download/');
+define('DIR_UPLOAD', 'F:\Program\xampp\htdocs\Foodsafe/system/upload/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Foodsafe/system/logs/');
+define('DIR_MODIFICATION', 'F:\Program\xampp\htdocs\Foodsafe/system/modification/');
+define('DIR_CATALOG', 'F:\Program\xampp\htdocs\Foodsafe/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Templates');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Foodsafe/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Foodsafe/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Foodsafe/catalog/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Foodsafe/system/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Foodsafe/catalog/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Foodsafe/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Foodsafe/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Foodsafe/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Foodsafe/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Foodsafe/system/download/');
+define('DIR_UPLOAD', 'F:\Program\xampp\htdocs\Foodsafe/system/upload/');
+define('DIR_MODIFICATION', 'F:\Program\xampp\htdocs\Foodsafe/system/modification/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Foodsafe/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'templates');
+define('DB_PREFIX', '');
