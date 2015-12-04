86c2ba451283b4cc052264fcf05187084d7eef58
Bommer <quangthi_90@yahoo.com.vn>
2015-12-05 00:59:20 +0700

    - Add news category page
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ catalog/language/vietnamese/news/category.php
@@ -0,0 +1,14 @@
+<?php
+/*------------------------------------------------------------------------
+# Advanced News System for OpenCart 1.5.x
+# ------------------------------------------------------------------------
+# Copyright (C) 2011 OpenCartSoft.com. All Rights Reserved.
+# @license - Copyrighted Commercial Software
+# Author: www.OpenCartSoft.com
+# Websites:  http://www.opencartsoft.com -  Email: admin@opencartsoft.com
+# This file may not be redistributed in whole or significant part.
+-------------------------------------------------------------------------*/
+
+// Text
+$_['text_news']      = 'Tin tức';
+?>
